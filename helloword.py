5d13f4145395d1958d49da150b2b9b4616bcefb0
raissacunhapeixoto <145492067+raissacunhapeixoto@users.noreply.github.com>
2023-11-30 15:18:46 -0300

    Create helloword.py
    fusrt

---
+++ helloword.py
@@ -0,0 +1,7 @@
+print("Hello Word") # Mostra na tela o texto Hello Word
+mensagem = "Ola, tudo bem?"
+print(mensagem) # mostra a mensagem na tela
+print(mensagem[1]) # mostra a segunda letra da mensagem
+print(mensagem[0:4]) # mostra da primeira ate a 4 letra mensagem
+print(mensagem[-1]) # mostra a ultima letra da mensagem
+print(mensagem[10:13])
